dd61724d127b0399b34e98353a2616b36e744639
jack <jack@jack-XPS-13-9343>
2018-02-26 18:32:23 +0800

    Change-Id: Id4b9a64612a0fe3b5c07229e16caddfa5b0ce02c Signed-off-by: jack <jack@jack-XPS-13-9343>

--- myspring/src/main/java/com/tgb/controller/UserController.java
+++ myspring/src/main/java/com/tgb/controller/UserController.java
@@ -1,11 +1,8 @@
 package com.tgb.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,6 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.alibaba.fastjson.JSONObject;
 import com.tgb.model.User;
 import com.tgb.service.UserService;
+import com.tgb.util.Config;
+import com.tgb.util.RSA;
 
 @Controller
 @RequestMapping("/user")
@@ -23,21 +22,6 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
-	
-	
-	
-  /**
-  * 用户信息跳转
-  * @param request
-  * @return
-  */
- @RequestMapping("/beginuser")
- public String beginlogin(HttpServletRequest request){
-         
-   return "user/user.index.html";
- }	
-	
 	
 	
 	/**
@@ -73,11 +57,12 @@ public class UserController {
 	 * @param request
 	 * @return
 	 */
-	@RequestMapping("/toAddUser")
-	public String toAddUser(HttpServletRequest request){
-		
-		return "/addUser";
-	}
+  @RequestMapping("/getAddUserPage")
+  public String getAddUserPage(HttpServletRequest request) {
+
+    return "user/user.add.html";
+  }
+  
 	/**
 	 * 添加用户并重定向
 	 * @param user
@@ -86,62 +71,53 @@ public class UserController {
 	 */
 	@RequestMapping("/addUser")
 	public String addUser(User user,HttpServletRequest request){
-		userService.save(user);
+		String username = user.getUsername();
+		String password = user.getPassword();
+  if(username!=null&&!username.equals("")&&password!=null&&!password.equals("")){
+      String sign = RSA.sign(password, Config.private_key, Config.input_charset);
+      user.setPassword(sign);
+      userService.save(user);
+    }
 		return "redirect:/user/getAllUser";
 	}
 	
-	/**
-	 *编辑用户
-	 * @param user
-	 * @param request
-	 * @return
-	 */
-	@RequestMapping("/updateUser")
-	public String updateUser(User user,HttpServletRequest request){
-		
-		
-		if(userService.update(user)){
-			user = userService.findById(user.getId());
-			request.setAttribute("user", user);
-			return "redirect:/user/getAllUser";
-		}else{
-			return "/error";
-		}
-	}
-	/**
-	 * 根据id查询单个用户
-	 * @param id
-	 * @param request
-	 * @return
-	 */
-	@RequestMapping("/getUser")
-	public String getUser(int id,HttpServletRequest request){
-		
-		request.setAttribute("user", userService.findById(id));
-		return "/editUser";
-	}
-	/**
-	 * 删除用户
-	 * @param id
-	 * @param request
-	 * @param response
-	 */
-	@RequestMapping("/delUser")
-	public void delUser(int id,HttpServletRequest request,HttpServletResponse response){
-		String result = "{\"result\":\"error\"}";
-		
-		if(userService.delete(id)){
-			result = "{\"result\":\"success\"}";
-		}
-		
-		response.setContentType("application/json");
-		
-		try {
-			PrintWriter out = response.getWriter();
-			out.write(result);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	 /**
+   * 跳转到添加用户界面
+   * @param request
+   * @return
+   */
+  @RequestMapping("/getEditUserPage")
+  public String getEditUserPage(int id,Model model) {
+    User user = userService.findById(id);
+    model.addAttribute("userinfo",user);
+    return "user/user.edit.html";
+  }
+	
+	 /**
+   * 添加用户并重定向
+   * @param user
+   * @param request
+   * @return
+   */
+  @RequestMapping("/editUser")
+  public String editUser(User user,HttpServletRequest request){
+    String username = user.getUsername();
+    if(username!=null&&!username.equals("")){
+      userService.update(user);
+          }
+    return "redirect:/user/getAllUser";
+  }
 
-	}
+	 /**
+   * 删除用户
+   * @param id
+   * @param request
+   * @param response
+   */
+  @RequestMapping("/delUser")
+  public String delUser(int id,HttpServletRequest request){
+    userService.delete(id);
+    return "redirect:/user/getAllUser";
+  }
+	
 }
--- myspring/src/main/java/com/tgb/mapper/UserMapper.java
+++ myspring/src/main/java/com/tgb/mapper/UserMapper.java
@@ -8,6 +8,7 @@ public interface UserMapper {
 
 	void save(User user);
 	boolean update(User user);
+	boolean updatePassword(User user);
 	boolean delete(int id);
 	User findById(int id);
 	User findByName(String username);
