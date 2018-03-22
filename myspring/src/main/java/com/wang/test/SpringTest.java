0068f64e55a83be1731f4413a120162519effeb7
jack <jack@jack-XPS-13-9343>
2018-03-22 10:18:50 +0800

    Change-Id: I84da322b4f5be4a69e59919f990e37a7d8273a77 Signed-off-by: jack <jack@jack-XPS-13-9343>

--- myspring/src/main/java/com/wang/test/SpringTest.java
+++ myspring/src/main/java/com/wang/test/SpringTest.java
@@ -9,13 +9,13 @@ import com.wang.model.User;
 public class SpringTest {
 
 	 public static void main(String[] args) {
-		 ApplicationContext ctx = new ClassPathXmlApplicationContext("config/spring-common.xml");
+		 ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-common.xml");
 		 UserMapper userMapper = (UserMapper) ctx.getBean("userMapper");
 		 System.out.println(userMapper);
 		   User user = new User();
 		   user.setUserage("18");
 		   user.setUsername("张三");
 		   //user.setId(123);
-		 userMapper.save(user);
+		 //userMapper.save(user);
 	 }
 }
